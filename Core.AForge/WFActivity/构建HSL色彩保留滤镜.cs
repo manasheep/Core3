a16a6b46f4883a26d11024da5300340968f6b39d
徐明璐 <icesee@126.com>
2014-11-05 01:45:17 +0800

    实现HSL滤镜功能

---
+++ Core.AForge/WFActivity/构建HSL色彩保留滤镜.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using AForge;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+
+namespace Core.AForge.WFActivity
+{
+    [Designer(typeof(HSL色彩保留滤镜参数输入))]
+    public sealed class 构建HSL色彩保留滤镜 : CodeActivity
+    {
+        [RequiredArgument]
+        public InArgument<Int32> 最低色相 { get; set; }
+        [RequiredArgument]
+        public InArgument<Int32> 最高色相 { get; set; }
+        [RequiredArgument]
+        public InArgument<Single> 最低饱和度 { get; set; }
+        [RequiredArgument]
+        public InArgument<Single> 最高饱和度 { get; set; }
+        [RequiredArgument]
+        public InArgument<Single> 最低亮度 { get; set; }
+        [RequiredArgument]
+        public InArgument<Single> 最高亮度 { get; set; }
+
+        public InArgument<HSL> 填充颜色 { get; set; }
+
+        [RequiredArgument]
+        public OutArgument<HSLFiltering> 构建的滤镜 { get; set; }
+
+        // 如果活动返回值，则从 CodeActivity<TResult>
+        // 派生并从 Execute 方法返回该值。
+        protected override void Execute(CodeActivityContext context)
+        {
+            var hslf = new HSLFiltering(new IntRange(context.GetValue(最低色相), context.GetValue(最高色相)), new Range(context.GetValue(最低饱和度), context.GetValue(最高饱和度)), new Range(context.GetValue(最低亮度), context.GetValue(最高亮度)));
+            var color = context.GetValue(填充颜色);
+            if (color != null) hslf.FillColor = color;
+            context.SetValue(构建的滤镜,hslf);
+        }
+    }
+}

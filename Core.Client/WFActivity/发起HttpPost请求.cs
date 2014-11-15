42ab9f061742ccef54ed3405ffd6df2a4034b691
徐明璐 <icesee@126.com>
2014-11-16 04:48:07 +0800

    新增Http相关功能

---
+++ Core.Client/WFActivity/发起HttpPost请求.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Activities;
+using System.Threading.Tasks;
+
+namespace Core.WFActivity
+{
+    [Designer(typeof(网址及Http内容输入))]
+    public sealed class 发起HttpPost请求 : AsyncCodeActivity
+    {
+        /// <summary>
+        /// 目标网址
+        /// </summary>
+        [RequiredArgument]
+        public InArgument<String> 网址 { get; set; }
+        [RequiredArgument]
+        public InArgument<HttpContent> Http内容 { get; set; }
+
+        [RequiredArgument]
+        public OutArgument<HttpResponseMessage> 响应消息 { get; set; }
+
+        protected override IAsyncResult BeginExecute(AsyncCodeActivityContext context, AsyncCallback callback, object state)
+        {
+            var hc = new HttpClient();
+            var task = hc
+                .PostAsync(context.GetValue(网址), context.GetValue(Http内容))
+                .ToApm(callback, state);
+            task.ContinueWith(q =>
+                    hc.Dispose()
+                );
+            return task;
+        }
+
+        protected override void EndExecute(AsyncCodeActivityContext context, IAsyncResult result)
+        {
+            context.SetValue(响应消息, (result as Task<HttpResponseMessage>).Result);
+        }
+    }
+}
---
+++ Core.Client/WFActivity/构建字符串形式的Http内容.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Activities;
+using Core.Net;
+
+namespace Core.WFActivity
+{
+    [Designer(typeof(构建字符串形式的Http内容参数输入))]
+    public sealed class 构建字符串形式的Http内容 : CodeActivity
+    {
+        [RequiredArgument]
+        public InArgument<String> 内容 { get; set; }
+        [RequiredArgument]
+        public InArgument<文件对应MIME类型> MIME类型 { get; set; }
+
+        [RequiredArgument]
+        public OutArgument<StringContent> 字符串形式的Http内容 { get; set; }
+
+        // 如果活动返回值，则从 CodeActivity<TResult>
+        // 派生并从 Execute 方法返回该值。
+        protected override void Execute(CodeActivityContext context)
+        {
+            var httpContent = new StringContent(context.GetValue(内容));
+            httpContent.Headers.ContentType = new MediaTypeHeaderValue(context.GetValue(MIME类型).GetDescription());
+            context.SetValue(字符串形式的Http内容, httpContent);
+        }
+    }
+}

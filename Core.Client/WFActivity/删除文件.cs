d41a0824592d09c4869bbd03fc1b5d4ec7adb56b
徐明璐 <icesee@126.com>
2014-11-17 05:56:47 +0800

    新增删除文件功能

---
+++ Core.Client/WFActivity/删除文件.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Activities;
+
+namespace Core.WFActivity
+{
+    [Designer(typeof(文件路径输入))]
+    public sealed class 删除文件 : CodeActivity
+    {
+        [RequiredArgument]
+        public InArgument<string> 文件路径 { get; set; }
+
+        // 如果活动返回值，则从 CodeActivity<TResult>
+        // 派生并从 Execute 方法返回该值。
+        protected override void Execute(CodeActivityContext context)
+        {
+            File.Delete( context.GetValue(文件路径));
+        }
+    }
+}
---
+++ Core.Client/WFActivity/文件路径输入.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Core.WFActivity
+{
+    // 文件路径输入.xaml 的交互逻辑
+    public partial class 文件路径输入
+    {
+        public 文件路径输入()
+        {
+            InitializeComponent();
+        }
+    }
+}

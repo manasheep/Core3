188fde0b171d886327b949dbf270216481f1a23f
SkyD <icesee@126.com>
2016-05-14 03:34:31 +0800

    新增UWP处理函数

---
+++ Core.UWP/UWP处理函数.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+
+namespace Core.UWP
+{
+    // ReSharper disable once InconsistentNaming
+    public static class UWP处理函数
+    {
+        public static void 复制到剪切板(string 内容)
+        {
+            DataPackage dataPackage = new DataPackage { RequestedOperation = DataPackageOperation.Copy };
+            dataPackage.SetText(内容);
+            Clipboard.SetContent(dataPackage);
+        }
+    }
+}
